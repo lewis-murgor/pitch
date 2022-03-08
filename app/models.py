66505f6a1807e94cb147cf25bf91dc8b52a1e255
Lewis <lewis.murgor@student.moringaschool.com>
2022-03-08 22:27:39 +0300

    create model classes

--- app/models.py
+++ app/models.py
@@ -1,9 +1,27 @@
 from . import db
+from datetime import datetime
+from werkzeug.security import generate_password_hash,check_password_hash
 
 class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer,primary_key = True)
     username = db.Column(db.String(255))
+    pitches = db.relationship('Pitch',backref = 'user',lazy="dynamic")
+    upvotes = db.relationship('UpVote',backref = 'user',lazy="dynamic")
+    downvotes = db.relationship('DownVote',backref = 'user',lazy="dynamic")
+    pass_secure = db.Column(db.String(255))
+
+    @property
+    def password(self):
+        raise AttributeError('You cannot read the password attribute')
+
+    @password.setter
+    def password(self, password):
+        self.pass_secure = generate_password_hash(password)
+
+
+    def verify_password(self,password):
+        return check_password_hash(self.pass_secure,password)
 
     def __repr__(self):
         return f'User {self.username}'
@@ -13,4 +31,43 @@ class Pitch(db.Model):
 
     id = db.Column(db.Integer,primary_key = True)
     title = db.Column(db.String(255))
-    
+    text = db.Column(db.String(255))
+    posted = db.Column(db.DateTime,default=datetime.utcnow)
+    category_id = db.Column(db.Integer,db.ForeignKey('categories.id'))
+    user_id = db.Column(db.Integer,db.ForeignKey('users.id'))
+    upvotes = db.relationship('UpVote',backref = 'pitch',lazy="dynamic")
+    downvotes = db.relationship('DownVote',backref = 'pitch',lazy="dynamic")
+
+    def __repr__(self):
+        return f'User {self.name}'
+
+class Category(db.Model):
+    __tablename__= 'categories'
+
+    id = db.Column(db.Integer,primary_key = True)
+    name = db.Column(db.String(255))
+    pitches = db.relationship('Pitch',backref = 'category',lazy="dynamic")
+
+    def __repr__(self):
+        return f'User {self.name}'
+
+class UpVote(db.Model):
+    __tablename__= 'upvotes'
+
+    id = db.Column(db.Integer,primary_key = True)
+    user_id = db.Column(db.Integer,db.ForeignKey('users.id'))
+    pitch_id = db.Column(db.Integer,db.ForeignKey('pitches.id'))
+
+    def __repr__(self):
+        return f'User {self.name}'
+
+class DownVote(db.Model):
+    __tablename__ = 'downvotes'
+
+    id = db.Column(db.Integer,primary_key = True)
+    user_id = db.Column(db.Integer,db.ForeignKey('users.id'))
+    pitch_id = db.Column(db.Integer,db.ForeignKey('pitches.id'))
+
+    def __repr__(self):
+        return f'User {self.name}'
+
