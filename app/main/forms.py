84f658c397c65f6c327848674d88afa4aa393b56
Lewis <lewis.murgor@student.moringaschool.com>
2022-03-10 22:32:03 +0300

    create pitch form

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField,TextAreaField,SubmitField
+from wtforms import StringField,TextAreaField,SubmitField,RadioField,SelectField
 from wtforms.validators import InputRequired
 
 class CommentForm(FlaskForm):
@@ -10,4 +10,11 @@ class CommentForm(FlaskForm):
 class UpdateProfile(FlaskForm):
     
     bio = TextAreaField('Tell us about you.',validators = [InputRequired()])
+    submit = SubmitField('Submit')
+
+class PitchForm(FlaskForm):
+
+    title = StringField('Title', validators=[InputRequired()])
+    text = TextAreaField('Add pitch', validators=[InputRequired()])
+    category_id = SelectField('Select Pitch Category', choices=[('1', 'Wise'), ('2', 'Pickup Lines'), ('3', 'Motivational')])
     submit = SubmitField('Submit')
