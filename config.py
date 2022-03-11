6ffb36ce5b94e0a60018fd1fdfd644570d4db134
Lewis <lewis.murgor@student.moringaschool.com>
2022-03-11 13:44:51 +0300

    deployment to heroku

--- config.py
+++ config.py
@@ -27,7 +27,7 @@ class ProdConfig(Config):
     Args:
         Config: The parent configuration class with General configuration settings
     '''
-    pass
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
 
 class TestConfig(Config):
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://lewis:lewis29@localhost/pitch_test'
