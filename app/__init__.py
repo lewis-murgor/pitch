0485dec50a70490605145acb22fa8567aa56abba
Lewis <lewis.murgor@student.moringaschool.com>
2022-07-13 17:25:41 +0300

    modify

--- app/__init__.py
+++ app/__init__.py
@@ -46,7 +46,6 @@ def create_app(config_name):
     # configure UploadSet
     configure_uploads(app,photos)
 
-    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
+    
 
     return app
