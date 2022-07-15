660662cd86fc91a57fd1bc3a252ddd149696d7eb
Lewis <lewis.murgor@student.moringaschool.com>
2022-07-15 20:28:16 +0300

    deploy

--- config.py
+++ config.py
@@ -27,7 +27,7 @@ class ProdConfig(Config):
     Args:
         Config: The parent configuration class with General configuration settings
     '''
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    SQLALCHEMY_DATABASE_URI = 'postgresql-clean-09518'
 
 class TestConfig(Config):
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://lewis:lewis29@localhost/pitch_test'
